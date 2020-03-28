703aaf74ecc4c8eb48f400761d712273279d5b84
Praveen Krishnan <praveen.k@qburst.com>
2020-03-28 18:43:21 +0530

    eslint updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,23 @@
-import * as React from "react";
-import "./App.css";
+import * as React from 'react';
+import './App.css';
 
 class App extends React.Component {
-    render(): JSX.Element {
-      return (
-            <div>
-                <h1>App.js</h1>
-            </div>
-        );
-    }
+  state = {
+    count: 0,
+  };
+
+  incrementCount = (): void => {
+    this.setState((prevState: any) => ({count: prevState.count + 1}));
+  };
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <h1>count : {this.state.count}</h1>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
 }
 
 export default App;
